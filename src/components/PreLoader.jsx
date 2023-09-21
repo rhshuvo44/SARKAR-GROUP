e69d3eb54252a15368e8ca3987f29aa759af46d2
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-09-21 18:10:50 +0600

    preloader

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const PreLoader = () => {
-    return (
-        <>
-            <div className="preloader">
-                <div className="preloader-status"></div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="se-pre-con">
+        <div className="preloader-wrapper">
+          <div className="preloader"></div>
+          <div className="preloader2"></div>
+        </div>
+      </div>
+      {/* <div className="preloader">
+        <div className="preloader-status"></div>
+      </div> */}
+    </>
+  );
 };
 
 export default PreLoader;
