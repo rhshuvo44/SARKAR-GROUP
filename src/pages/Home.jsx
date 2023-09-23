82d7e94a25c5418863e5f0892d53f0f611e57e36
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-09-23 15:19:16 +0600

    home pages partner section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
       {/* <TeamHeader />
       <Team /> */}
       <Partner/>
-      <Testimonial />
+      {/* <Testimonial /> */}
       {/* <LatestBlog /> */}
       <Footer />
     </>
