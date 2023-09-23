5b5d0efa1319400a10205461e3ac25ecbbad0786
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-09-23 13:04:23 +0600

    home pages project gallery section

--- src/components/LoadMoreProduct.jsx
+++ src/components/LoadMoreProduct.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
+import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 
 const LoadMoreProduct = () => {
-    return (
-        <>
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="gallery-load-more-btn">
-                        <Link to="#">Load More Project <i className="icofont-thin-double-right"></i></Link>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="gallery-load-more-btn">
+            <Link to="#">
+              Load More Project <i className="icofont-thin-double-right"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoadMoreProduct;
