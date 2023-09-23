855bf8249649f7ad411c531ec9f007171bef2d62
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-09-23 14:29:54 +0600

    contact pages map section

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,46 +1,55 @@
-import React from 'react';
+import React from "react";
 
 const ContactInfo = () => {
-    return (
-        <>
-            <div className="contact-info">
-                <div className="contact-info-item">
-                    <div className="contact-info-icon">
-                        <i className="icofont-map-pins"></i>
-                    </div>
-                    <div className="contact-info-text">
-                        <h2>address</h2>
-                        <span>Californiya , United State</span>
-                    </div>
-                </div>
-            </div>
-            <div className="contact-info">
-                <div className="contact-info-item">
-                    <div className="contact-info-icon">
-                        <i className="icofont-email"></i>
-                    </div>
-                    <div className="contact-info-text">
-                        <h2>e-mail</h2>
-                        <span> <a href="mailto:company@gmail.com">company@gmail.com</a> </span>
-                        <span> <a href="mailto:yourmail@gmail.com">yourmail@gmail.com</a> </span>
-                    </div>
-                </div>
-            </div>
-            <div className="contact-info">
-                <div className="contact-info-item">
-                    <div className="contact-info-icon">
-                        <i className="icofont-wall-clock"></i>
-                    </div>
-                    <div className="contact-info-text">
-                        <h2>office time</h2>
-                        <span>Mon - Thu 9:00 am - 4.00 pm</span>
-                        <span>Thu - Sat 10.00 pm - 5.00 pm</span>
-                        <span>Sunday Office Holiday</span>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="contact-info">
+        <div className="contact-info-item">
+          <div className="contact-info-icon">
+            <i className="icofont-map-pins"></i>
+          </div>
+          <div className="contact-info-text">
+            <h2>address</h2>
+            <span>
+              6/A S.K. Road, Friends Market, 5th Floor ,Narayanganj-1400
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="contact-info">
+        <div className="contact-info-item">
+          <div className="contact-info-icon">
+            <i className="icofont-email"></i>
+          </div>
+          <div className="contact-info-text">
+            <h2>e-mail</h2>
+            <span>
+              <a href="mailto:info@sarkargroupofcompanies.com">
+                info@sarkargroupofcompanies.com
+              </a>
+            </span>
+            <span>
+              <a href="mailto:sarkarengineeringworks017@gmail.com">
+                sarkarengineeringworks017@gmail.com
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="contact-info">
+        <div className="contact-info-item">
+          <div className="contact-info-icon">
+            <i className="icofont-wall-clock"></i>
+          </div>
+          <div className="contact-info-text">
+            <h2>office time</h2>
+            <span>Sat - Thu 10:00 am - 6.00 pm</span>
+            <span>Friday Office Holiday</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ContactInfo;
