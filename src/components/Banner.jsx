3c443fea15a04bc2472c424d88032821a1ad8372
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-10-10 17:47:44 +0600

    banner side delay up

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
             spaceBetween={50}
             slidesPerView={1}
             autoplay={{
-              delay: 2500,
+              delay: 3000,
               disableOnInteraction: false,
             }}
             loop={true}
